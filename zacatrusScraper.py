a58d8366b51432fe702ebbafc94131d1f81d8b78
jmontufo <61506867+jmontufo@users.noreply.github.com>
2021-03-27 19:38:30 +0100

    Treball inicial realitzat
    Obtenció dels enllaços de les pàgines de categories i de jocs

---
+++ zacatrusScraper.py
@@ -0,0 +1,97 @@
+from urllib.request import Request, urlopen, URLError, re, urlparse
+import datetime
+import time
+
+class Throttle:
+    """Add a delay between downloads to the same domain
+    """
+    def __init__(self, delay):
+        # amount of delay between downloads for each domain
+        self.delay = delay
+        # timestamp of when a domain was last accessed
+        self.domains = {}
+    
+    def wait(self, url):
+        domain = urlparse(url).netloc
+        last_accessed = self.domains.get(domain)
+        if self.delay > 0 and last_accessed is not None:
+            sleep_secs = self.delay - (datetime.datetime.now() - last_accessed).seconds
+            if sleep_secs > 0:
+                # domain has been accessed recently
+                # so need to sleep
+                time.sleep(sleep_secs)
+        # update the last accessed time
+        self.domains[domain] = datetime.datetime.now()
+ 
+def download(url, user_agent='PracticaUOC/jmontufo', num_retries=2):
+    print('Downloading:', url)
+    headers = {'User-agent': user_agent}
+    request = Request(url, headers=headers)
+    try:
+        html = urlopen(request).read()
+        html = html.decode('utf-8')
+    except URLError as e:
+        print('Download error:', e.reason)
+        html = None
+        if num_retries > 0:
+            if hasattr(e, 'code') and 500 <= e.code < 600:
+                # recursively retry 5xx HTTP errors
+                return download(url, user_agent, num_retries-1)
+    return html
+
+def crawl_sitemap(url, max_downloaded_pages = 1000000):
+    downloaded_pages = 0
+    # download the sitemap file
+    sitemap = download(url)
+    # extract the sitemap links
+    links = re.findall('<loc>(.*?)</loc>', sitemap)
+    # download each link
+    for link in links:
+        if downloaded_pages > max_downloaded_pages:
+            return
+        print(download(link))
+        downloaded_pages = downloaded_pages + 1
+        
+def link_crawler(seed_url, link_regex, delay = 5, max_depth=2, max_downloaded_pages = 1000000):
+    """Crawl from the given seed URL following links matched by link_regex
+    """    
+    crawl_queue = [seed_url]
+    seen = {}
+    seen[seed_url] = 0
+    downloaded_pages = 0
+    throttle = Throttle(delay)
+
+    while crawl_queue and downloaded_pages < max_downloaded_pages:
+        url = crawl_queue.pop()
+        
+        depth = seen[url]
+        if depth != max_depth:
+        
+            throttle.wait(url)
+            html = download(url)
+            
+            downloaded_pages = downloaded_pages + 1
+            
+            # filter for links matching our regular expression
+            for link in get_links(html):
+                if re.match(link_regex, link):
+                    # check if have already seen this link
+                    if link not in seen:
+                        seen[link] = depth + 1
+                        crawl_queue.append(link)
+                
+def get_links(html):
+    """Return a list of links from html
+    """
+    # a regular expression to extract all links from the webpage
+    webpage_regex = re.compile('<a[^>]+href=["\'](.*?)["\']', re.IGNORECASE)
+    # list of all links from the webpage
+    return webpage_regex.findall(html)
+
+#print(download('http://www.zacatrus.com'))
+#crawl_sitemap('https://zacatrus.es/pub/media/sitemap.xml',10)
+
+link_crawler('https://zacatrus.es/juegos-de-mesa.html', 'https://zacatrus\.es/[^/]*\.html$', 5, 3, 10)
+
+# if re.match('https://www.zacatrus.es/*', 'https://zacatrus.es/juegos-de-mesa/para_2.html'):
+#     print('holi')
